60707d1911eddf02a4b03771e3c47d5ec41514c3
manoelabms <manoelabusatto04@gmail.com>
2022-05-09 11:19:51 -0300

    add funcoes

---
+++ funcoes.py
@@ -0,0 +1,35 @@
+#sorteando paises
+import random 
+def sorteia_pais(dicionario):
+    lista_paises = []
+    for paises in dicionario.keys():
+        lista_paises.append(paises)
+    return random.choice(lista_paises)
+
+#distancia de haversine   
+from math import *
+def haversine(r, x1, y1, x2, y2):
+    d = 2*r*asin(((sin((radians(x2)-radians(x1))/2))**2 + cos(radians(x1))*cos(radians(x2))*(sin((radians(y2)-radians(y1))/2)**2))**(1/2))
+    return d
+
+#adicionando em uma lista ordenada
+def adiciona_em_ordem(pais, distancia, lista):
+    i = 0
+    if lista == []:
+        return [[pais, distancia]]
+    for lista_paises in lista:
+        if lista_paises[0] == pais:
+            return lista
+        if lista_paises[1] > distancia:
+            lista.insert(i, [pais, distancia])
+            break
+        i += 1
+    return lista
+
+#esta na lista?
+def esta_na_lista(pais, lista):
+    for paises in lista:
+        if pais in paises:
+            return True
+    return False
+    
