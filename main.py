8a908aac092d4de60ab531ada0a3fa38bd3f4ae3
manoelabms <manoelabusatto04@gmail.com>
2022-05-10 13:20:37 -0300

    comecinho do nosso codigo principal

---
+++ main.py
@@ -0,0 +1,13 @@
+# esses comandos estavam no ex de jogo que o humberto disponibilizou
+from normalizando_bases import DADOS
+
+
+print('Comandos possíveis: /n -dica: entra no mercado de dicas /n -desisto: desiste da rodada /n -inventario: exibe sua posição')
+print('Voce tem 20 tentativas')
+pais_ou_dica = input('Insira seu palpite')
+
+while pais_ou_dica != 'dica':
+    if pais_ou_dica not in #dados normalizados:
+        print('país desconhecido')
+    
+    pais_ou_dica = input('Insira seu palpite')
