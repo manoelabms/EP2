c4b96c2a2b8227dc3d3683d238ad941ab39e4301
manoelabms <manoelabusatto04@gmail.com>
2022-05-11 14:46:33 -0300

    contador tentativas e lista tentativas

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-# esses comandos estavam no ex de jogo que o humberto disponibilizou
 print('Comandos possíveis: \n -dica: entra no mercado de dicas \n -desisto: desiste da rodada \n -inventario: exibe sua posição')
-print('Você tem 20 tentativas')
+print('Você tem {} tentativas'.format(contador_tentativas))
 pais_ou_dica = input('Insira seu palpite')
+
+contador_tentativas = 20
+lista_tentativas = [] #nessa lista devem ser adicionados os paises de forma crescente pela distancia de haversine
+
